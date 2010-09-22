040c93c1d982935f1138787af572257e377c9d7f
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2010-09-22 17:28:37 +0100

    zdb bug

--- lib/szpc/zdb.rb
+++ lib/szpc/zdb.rb
@@ -9,8 +9,8 @@ module Zimbra
     def get_mails_from_folders( db, user, folder_ids, volumes, min_date = nil )
       sql = <<-SQL
         SELECT id, size, volume_id, mailbox_id, mod_content, date, '' AS relative_path , '' AS absolute_path
-        FROM #{mailbox_name}.mail_item 
-        WHERE mailbox_id=#{::Zimbra::Users.mailbox_group(user)} 
+        FROM #{Zimbra::Users.mailbox_group(user)}.mail_item 
+        WHERE mailbox_id=#{::Zimbra::Users.mailbox_id(user)} 
           AND type = #{::Zimbra::Folders::TYPES[:MAIL]} 
           AND folder_id IN (#{folder_ids.join(', ')} )
       SQL
